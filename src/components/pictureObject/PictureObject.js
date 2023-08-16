77ccd86fd055f8f9265783f84ef5e05420a4aca4
mgblanco10 <mgblanco1001@gmail.com>
2023-08-16 16:06:33 +0200

    refactor picturespage

--- src/components/pictureObject/PictureObject.js
+++ src/components/pictureObject/PictureObject.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import './PictureObject.css'
 
-
-const PictureObject = ({ pictures }) => {
+const PictureObject = ({picture}) => {
   return (
-    <div>
-      <h3>Imágenes:</h3>
+    <div className='container'>
       <ul className="container-PintureObject">
-        {pictures.map((picture) => (
           <li key={picture.id}>
             <p className="id-img">ID: {picture.id}</p>
             <p className="autor-img">Autor: {picture.author}</p>
             <img className="size-img"src={picture.download_url} alt={`Imagen por ${picture.author}`} />
           </li>
-        ))}
       </ul>
     </div>
   );
